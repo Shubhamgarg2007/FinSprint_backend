07fd548da1028de76a29cb15f5d961a2101fb6d1
Shubhamgarg2007 <127992990+Shubhamgarg2007@users.noreply.github.com>
2025-05-12 22:51:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ app = FastAPI()
 # Allow requests from your frontend (adjust the origin if needed)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # frontend dev server
+    allow_origins=["https://fin-sprint-en2l.vercel.app/"],  # frontend dev server
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
